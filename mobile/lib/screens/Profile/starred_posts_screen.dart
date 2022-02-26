ac9874a933b9e9d96a22884ae5ffa118450df229
lwasinam <lwasinamdilli@gmail.com>
2022-02-26 03:41:23 +0100

    added state management to curated post screen

--- mobile/lib/screens/Profile/starred_posts_screen.dart
+++ mobile/lib/screens/Profile/starred_posts_screen.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:get/get_core/src/get_main.dart';
 import 'package:get/get_instance/src/extension_instance.dart';
 import 'package:get/get_state_manager/src/rx_flutter/rx_getx_widget.dart';
@@ -7,7 +5,6 @@ import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
 import 'package:mobile/controller/loggedUserController.dart';
 import 'package:mobile/screens/Profile/controllers/profileController.dart';
 import 'package:mobile/screens/imports.dart';
-
 
 class starredPosts extends StatefulWidget {
   String? username;
@@ -19,14 +16,13 @@ class starredPosts extends StatefulWidget {
   _starredPostsState createState() => _starredPostsState();
 }
 
-
-
 ProfileController post = ProfileController();
+
 class _starredPostsState extends State<starredPosts> {
   @override
   void dispose() {
     super.dispose();
-   // Get.delete<UserPostsController>(tag: widget.tag);
+    // Get.delete<UserPostsController>(tag: widget.tag);
   }
 
   @override
@@ -34,7 +30,7 @@ class _starredPostsState extends State<starredPosts> {
     super.initState();
 
     post = Get.put(ProfileController(controllerusername: widget.username),
-         tag: widget.tag);
+        tag: widget.tag);
   }
 
   @override
@@ -42,80 +38,79 @@ class _starredPostsState extends State<starredPosts> {
     //  post = Get.put(UserPostsController(username: widget.username),
     //       tag: widget.tag);
 
-    return  GetX<ProfileController>(
-      init: ProfileController(controllerusername: widget.username),
-      tag: widget.tag,
-      builder: (text) {
-        return text.isLoading.value
-            ? SliverToBoxAdapter(
-                child: Center(
-                    child:
-                        SpinKitFadingCircle(color: tualeOrange.withOpacity(0.75))))
-            : text.profileInfo.value.starredPosts!.isEmpty
-                ? SliverToBoxAdapter(
-                    child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      SizedBox(
-                        height: 120.h,
-                      ),
-                      Text("No post"),
-                    ],
-                  ))
-                : SliverPadding(
-                    padding: const EdgeInsets.only(left: 5, right: 5),
-                    sliver: SliverGrid(
-
-                        //physics: NeverScrollableScrollPhysics(),
-
-                        gridDelegate:
-                            const SliverGridDelegateWithFixedCrossAxisCount(
-                          // mainAxisExtent: 5,
-                          crossAxisCount: 3,
+    return GetX<ProfileController>(
+        init: ProfileController(controllerusername: widget.username),
+        tag: widget.tag,
+        builder: (text) {
+          return text.isLoading.value
+              ? SliverToBoxAdapter(
+                  child: Center(
+                      child: SpinKitFadingCircle(
+                          color: tualeOrange.withOpacity(0.75))))
+              : text.profileInfo.value.starredPosts!.isEmpty
+                  ?  SliverToBoxAdapter(
+                      child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        SizedBox(
+                          height: 120.h,
                         ),
-                        delegate: SliverChildBuilderDelegate(
-                          (BuildContext context, int index) {
-                           
-                            return GestureDetector(
-                              onTap: () {
-                                Navigator.push(
-                                    context,
-                                    PageTransition(
-                                        type: PageTransitionType.topToBottom,
-                                        child: discoverScreen(index: index)));
-                              },
-                              child: Container(
+                        Text("No post"),
+                      ],
+                    ))
+                  : SliverPadding(
+                      padding: const EdgeInsets.only(left: 5, right: 5),
+                      sliver: SliverGrid(
+
+                          //physics: NeverScrollableScrollPhysics(),
+
+                          gridDelegate:
+                              const SliverGridDelegateWithFixedCrossAxisCount(
+                            // mainAxisExtent: 5,
+                            crossAxisCount: 3,
+                          ),
+                          delegate: SliverChildBuilderDelegate(
+                            (BuildContext context, int index) {
+                              return GestureDetector(
+                                onTap: () {
+                                  Navigator.push(
+                                      context,
+                                      PageTransition(
+                                          type: PageTransitionType.topToBottom,
+                                          child: discoverScreen(index: index)));
+                                },
                                 child: Container(
-                                  height: 100.h,
-                                  width: 100.h,
+                                  child: Container(
+                                    height: 100.h,
+                                    width: 100.h,
+                                    decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(8),
+                                        gradient: const LinearGradient(
+                                          begin: AlignmentDirectional(0.5, 0.5),
+                                          end: AlignmentDirectional(0.5, 1.9),
+                                          colors: [
+                                            Colors.transparent,
+                                            Colors.black87
+                                          ],
+                                        )),
+                                  ),
                                   decoration: BoxDecoration(
                                       borderRadius: BorderRadius.circular(8),
-                                      gradient: const LinearGradient(
-                                        begin: AlignmentDirectional(0.5, 0.5),
-                                        end: AlignmentDirectional(0.5, 1.9),
-                                        colors: [
-                                          Colors.transparent,
-                                          Colors.black87
-                                        ],
-                                      )),
+                                      image: DecorationImage(
+                                          fit: BoxFit.cover,
+                                          image: NetworkImage(text.profileInfo
+                                              .value.starredPosts![index]))),
+                                  margin: EdgeInsets.fromLTRB(2, 2, 2, 2),
+                                  height: 100.h,
+                                  width: 100.h,
                                 ),
-                                decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(8),
-                                    image: DecorationImage(
-                                        fit: BoxFit.cover,
-                                        image: NetworkImage(
-                                            text.profileInfo.value.starredPosts![index]))),
-                                margin: EdgeInsets.fromLTRB(2, 2, 2, 2),
-                                height: 100.h,
-                                width: 100.h,
-                              ),
-                            );
-                          },
-                          childCount: text.profileInfo.value.starredPosts!.length,
-                        )),
-                  );
-      }
-    );
+                              );
+                            },
+                            childCount:
+                                text.profileInfo.value.starredPosts!.length,
+                          )),
+                    );
+        });
   }
 }
