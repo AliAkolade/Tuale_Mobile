77b18bb4004c7dfb51f5e9142f71bd69d68d4c06
lwasinam <lwasinamdilli@gmail.com>
2022-04-01 01:24:00 +0100

    profile changes

--- mobile/lib/utils/secrets.dart
+++
@@ -1,5 +0,0 @@
-const String twilioKey = '3eed694396ac1ff60587ea330646ffd4';
-const String serviceID = 'VAb1899cf594ab102723a39403110cd491';
-const String accountSID = 'AC9dc1570f1a5dc00f7a27e6008525c246';
-const String mixPanelToken = '85afc0bc81026f14839f55b6c8abf0f3';
-const String hostAPI = 'https://tuale-mobile-api.herokuapp.com/api/v1/';
