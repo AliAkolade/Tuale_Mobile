062c8706e344b47a4c9bd957ab7b090bcea3ab14
lwasinam <lwasinamdilli@gmail.com>
2022-09-01 21:01:47 +0100

    video thumnbail

--- mobile/lib/screens/Profile/allPosts.dart
+++ mobile/lib/screens/Profile/allPosts.dart
@@ -6,6 +6,7 @@ import 'package:get/get_state_manager/src/simple/get_state.dart';
 import 'package:mobile/screens/Home/one_post_page.dart';
 import 'package:mobile/screens/Profile/controllers/userPostsController.dart';
 import 'package:mobile/screens/imports.dart';
+import 'package:video_thumbnail/video_thumbnail.dart';
 
 class AllPosts extends StatefulWidget {
   String? username;
@@ -39,96 +40,126 @@ class _AllPostsState extends State<AllPosts> {
     //  post = Get.put(UserPostsController(username: widget.username),
     //       tag: widget.tag);
 
-    return  GetX<UserPostsController>(
-      init: UserPostsController(username: widget.username),
-     // tag: widget.tag,
-      builder: (text) {
-        return text.isLoading.value
-            ? SliverToBoxAdapter(
-                child: Center(
-                    child:
-                        SpinKitFadingCircle(color: tualeOrange.withOpacity(0.75))))
-            : text.posts.length == 0
-                ? SliverToBoxAdapter(
-                    child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      SizedBox(
-                        height: 120.h,
-                      ),
-                      Text("No post"),
-                    ],
-                  ))
-                : SliverPadding(
-                    padding: const EdgeInsets.only(left: 5, right: 5),
-                    sliver: SliverGrid(
-
-                        //physics: NeverScrollableScrollPhysics(),
-
-                        gridDelegate:
-                            const SliverGridDelegateWithFixedCrossAxisCount(
-                          // mainAxisExtent: 5,
-                          crossAxisCount: 3,
+    return GetX<UserPostsController>(
+        init: UserPostsController(username: widget.username),
+        // tag: widget.tag,
+        builder: (text) {
+          return text.isLoading.value
+              ? SliverToBoxAdapter(
+                  child: Center(
+                      child: SpinKitFadingCircle(
+                          color: tualeOrange.withOpacity(0.75))))
+              : text.posts.length == 0
+                  ? SliverToBoxAdapter(
+                      child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        SizedBox(
+                          height: 120.h,
                         ),
-                        delegate: SliverChildBuilderDelegate(
-                          (BuildContext context, int index) {
-                            print(text.posts.length);
-                            return GestureDetector(
-                              onTap: () {
-                                Navigator.push(
-                                    context,
-                                    PageTransition(
-                                        type: PageTransitionType.topToBottom,
-                                        child: OnePost(
-                                            postMedia: text.posts.value[index].postMedia,
-                                            mediaType: text.posts.value[index].mediaType,
+                        Text("No post"),
+                      ],
+                    ))
+                  : SliverPadding(
+                      padding: const EdgeInsets.only(left: 5, right: 5),
+                      sliver: SliverGrid(
+
+                          //physics: NeverScrollableScrollPhysics(),
+
+                          gridDelegate:
+                              const SliverGridDelegateWithFixedCrossAxisCount(
+                            // mainAxisExtent: 5,
+                            crossAxisCount: 3,
+                          ),
+                          delegate: SliverChildBuilderDelegate(
+                            (BuildContext context, int index) {
+                              if (text.posts[index].mediaType != 'image') {}
+                              return GestureDetector(
+                                onTap: () {
+                                  Navigator.push(
+                                      context,
+                                      PageTransition(
+                                          type: PageTransitionType.topToBottom,
+                                          child: OnePost(
+                                            postMedia: text
+                                                .posts.value[index].postMedia,
+                                            mediaType: text
+                                                .posts.value[index].mediaType,
                                             id: text.posts.value[index].id,
                                             pageType: false,
-                                        )
-                                    ));
-                              },
-                              child:text.posts[index].mediaType != 'image' ?
-                              Container(
-                                   margin: EdgeInsets.fromLTRB(2, 2, 2, 2),
-                                height: 100.h,
-                                width: 100.h,
-                                color: Colors.black,
-                                child: Icon(Icons.play_arrow_outlined,
-                                color: Colors.white
-                                )
-                              )  :
-                              Container(
-                                child: Container(
-                                  height: 100.h,
-                                  width: 100.h,
-                                  decoration: BoxDecoration(
-                                      borderRadius: BorderRadius.circular(8),
-                                      gradient: const LinearGradient(
-                                        begin: AlignmentDirectional(0.5, 0.5),
-                                        end: AlignmentDirectional(0.5, 1.9),
-                                        colors: [
-                                          Colors.transparent,
-                                          Colors.black87
-                                        ],
-                                      )),
-                                ),
-                                decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(8),
-                                    image: DecorationImage(
-                                        fit: BoxFit.cover,
-                                        image: NetworkImage(
-                                            text.posts[index].postMedia))),
-                                margin: EdgeInsets.fromLTRB(2, 2, 2, 2),
-                                height: 100.h,
-                                width: 100.h,
-                              ),
-                            );
-                          },
-                          childCount: text.posts.length,
-                        )),
-                  );
-      }
+                                          )));
+                                },
+                                child: text.posts[index].mediaType != 'image'
+                                    ? VideoThumbnailWidget(
+                                      controller: text.posts[index].postMedia,
+                                    )
+                                    : Container(
+                                        child: Container(
+                                          height: 100.h,
+                                          width: 100.h,
+                                          decoration: BoxDecoration(
+                                              borderRadius:
+                                                  BorderRadius.circular(8),
+                                              gradient: const LinearGradient(
+                                                begin: AlignmentDirectional(
+                                                    0.5, 0.5),
+                                                end: AlignmentDirectional(
+                                                    0.5, 1.9),
+                                                colors: [
+                                                  Colors.transparent,
+                                                  Colors.black87
+                                                ],
+                                              )),
+                                        ),
+                                        decoration: BoxDecoration(
+                                            borderRadius:
+                                                BorderRadius.circular(8),
+                                            image: DecorationImage(
+                                                fit: BoxFit.cover,
+                                                image: NetworkImage(text
+                                                    .posts[index].postMedia))),
+                                        margin: EdgeInsets.fromLTRB(2, 2, 2, 2),
+                                        height: 100.h,
+                                        width: 100.h,
+                                      ),
+                              );
+                            },
+                            childCount: text.posts.length,
+                          )),
+                    );
+        });
+  }
+}
+
+class VideoThumbnailWidget extends StatelessWidget {
+  String? controller;
+
+  VideoThumbnailWidget({this.controller});
+
+  String? fileName;
+
+  Future<String> getThumbnail() async {
+    fileName = await VideoThumbnail.thumbnailFile(
+      video: controller!,
+      // thumbnailPath: (await getTemporaryDirectory()).path,
+      imageFormat: ImageFormat.WEBP,
+      maxHeight:
+          64, // specify the height of the thumbnail, let the width auto-scaled to keep the source aspect ratio
+      quality: 75,
     );
+    print(fileName);
+    return fileName!;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    getThumbnail();
+    return Container(
+        margin: EdgeInsets.fromLTRB(2, 2, 2, 2),
+        height: 100.h,
+        width: 100.h,
+        color: Colors.black,
+        child: Icon(Icons.play_arrow_outlined, color: Colors.white));
   }
 }
--- mobile/lib/screens/imports.dart
+++ mobile/lib/screens/imports.dart
@@ -6,7 +6,7 @@ export 'dart:typed_data';
 
 export 'package:flutter/material.dart';
 //export 'package:firebase_phone_auth_handler/firebase_phone_auth_handler.dart';
-export 'package:camera/camera.dart';
+// export 'package:camera/camera.dart';
 export 'package:dio/dio.dart';
 export 'package:flutter/material.dart';
 export 'package:flutter/services.dart';
